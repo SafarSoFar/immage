f180617039b0270902af122f5b93d06a76c72c56
SafarSoFar <cafar0525@gmail.com>
2024-07-03 10:39:17 +0500

    first attempt to change pixel colors and wirte it to image buff

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ greeting.pack()
 #img = PhotoImage(file="funny-cat.jpg")
 #canvas.create_image(img)
 image = Image.open("funny-cat.jpg")
+pixels = list(image.getdata())
+width, height = image.size
+
+# reading pixel values
+#for x in range (width):
+    #for y in range (height):
+        # -> print(pixels[x][2])
+
+#for [x,y] in range(pixels):
+    #print(x,y)
+#image = Image.frombytes(size=[width,height], data=pixels, mode=w)
+new_pixels = list()
+for pix in pixels:
+    new_pixels.append((pix[0], pix[1],255))
+    #print(pix)
+
+#image = Image.new(mode=image.mode, size=image.size)
+image.putdata(data=new_pixels)
 image = ImageTk.PhotoImage(image)
 img_label = Label(window, image=image)
 img_label.pack()
